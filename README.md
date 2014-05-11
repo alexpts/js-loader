66315de8055dec9aee52e92e190b35e21fd9ec76
alexpts <alexpts@yandex.ru>
2014-05-11 13:51:15 +0400

    add files

--- README.md
+++ README.md
@@ -1,4 +1,97 @@
-js-loader
-=========
+# Async loader javascript/css (json/html/ejs/...)
 
-Async javascript loader js and css
+#EN
+...
+
+#RU
+
+Асинхронный кроссдоменный загрузчик статических ресурсов. Позволяет подгружать зависимости, управлять имеющимися зависимостяси и вызывать функции обратного вызова в момент загрузки зависимости(ей). Что позволяет сократить время загрузки страницы и ускорить GUI сайта, сократив время загрузки страницы и время на разбор и применение неиспользуемых частей js и css.
+
+## Загрузка скриптов
+##### Иницилизаци загрузчика
+Загрузчик добавляет метод в глобальное пространтсво имен window.Loader предоставляя функцию конструктор, которая будет возвращать объект загрузчика.
+Конструктор может принять объект с параметрами:
+
+    moduleDir [""] - путь для поиска относительных модулей
+    addFromPage [true] - добавляет в момент иницилизации зпгрузчика все, имеющиеся на странице, js и css в загрузчик
+
+##### Пример иницилизации загрузчика
+
+    var Loader = new Loader({
+        'moduleDir': '/modules',
+        'addFromPage': false
+    });
+    
+В большинстве случаев достаточно всего 1 загрузчика для всех типов данных, поэтому можно перезатереть констурктор window.Loader экзкмпляром загрузчика, чтобы не создать еще одну копию загрузчика. Функция конструтокр реализована по патерну "модуль", предоставляя наружу только необходимое api, скрывая в себе сложность реализации.
+
+##### Пример загрузки одиночного скрипта:
+
+    Loader.load({String}name, {Object}[params], {Function}[callback]); 
+    
+    // загрузка по относительному пути
+    Loader.load('bootstrap.js'); 
+    Loader.load('bootstrap.css'); 
+    
+    // кросдоменная загрузка
+    Loader.load('bootstrap.js', {
+        url: 'http://yandex.st/bootstrap/3.1.1/js/bootstrap.min.js'
+    }); 
+    
+    //кросдоменная загрузка по полному пути
+    Loader.load('http://yandex.st/bootstrap/3.1.1/js/bootstrap.min.js');
+
+Параметр ***params*** полностью поддерживает все опции пердоставляемые jQuery.ajax(***params***) [http://stage.api.jquery.com/jQuery.ajax/];
+Поэтому можно повесить дополнительынй callback на любое состояние запроса и использовать почти все возможности ajax.
+
+##### Вызов callback после загрузки скрипта
+
+    Loader.load('bootstrap.js', function(){
+        alert('bootstrap is ready!');
+    }); 
+    
+##### Тонкое управление (сложный уровень, не рекомендуется для использования)
+    
+    Loader.load('bootstrap.js', {
+        success: function(data, textStatus, jqXHR ){
+            console.log('seccond  success callback');
+        },
+        complete: function(jqXHR, textStatus ){
+            console.log('complete callback');
+        },
+        statusCode: {
+            201: function(){
+                console.log('last collback  (status=201)');
+            }
+        }
+    }, function(){
+        console.log('first success callback');
+    });
+
+##### Пимер загрузки нескольких зависимостей:
+
+    Loader.loads({Array}modules, {Function}[callback]);
+
+    var dojoModule = {
+        name: 'dojo',
+        params: {
+            url: 'http://yandex.st/dojo/1.9.1/dojo/dojo.js'
+        }
+    };
+    Loader.loads([
+        'bootstrap.js', 
+        'bootstrap.css', 
+        'http://yandex.st/d3/3.4.5/d3.js', 
+        dojoModule
+        ], 
+        function(){
+            console.log('after load all callback');
+    });
+
+Я так и не смог однозначно определиться с тербованиями для автоматической подгрузки зависимостей из-за специфики архитектуры проекта, поэтому предоставил всего лишь 1 метод loads, который скорее всего пригодится при реализации автоматической подгрузки зависимостей в рамках той или иной архитектуры проекта.
+
+##### События
+Загрузчик выкидывает на элементе *document* событие ***Loader.load***, передавая в обработчик 3 параметра [name, extension, url]
+
+    $(document).on('Loader.load', function(event, name, extension, url){
+        ...
+    });
---
+++ src/loader.js
@@ -0,0 +1,289 @@
+// Loader
+// http://alexpts.ru
+// (c) 2011-2014 Alexpts
+// Loader may be freely distributed under the MIT license
+
+(function($, window){
+    'use strict';
+
+    var $head = $('head');
+    var $document = $(document);
+
+    function _createNode(name, attrs) {
+        var node = document.createElement(name), attr;
+
+        for(attr in attrs) {
+            if(attrs.hasOwnProperty(attr)) {
+                node.setAttribute(attr, attrs[attr]);
+            }
+        }
+
+        return node;
+    }
+
+    function _getExt(url){
+        return url.replace(/.*\.(\w+)$/, '$1').toLowerCase(); // .js, .css, .json, .tpl
+    }
+
+    function _lazyScriptTransport(s, originalOptions, jqXHR) {
+        var ext = _getExt(s.url), node;
+
+        return {
+            send: function(headers, callback ) {
+
+                // @todo add api to extend types
+                switch(ext) {
+                    case 'js':
+                        node = _createNode('script', {'src': s.url});
+                        break;
+                    case 'css':
+                        node = _createNode('link', {'href': s.url, 'rel': 'stylesheet'});
+                        break;
+                    default :
+                        console.log("Unknow extension module");
+                        jqXHR.abort();
+                        return false;
+                }
+
+                node.charset = s.scriptCharset || "utf-8";
+
+                node.onload = node.onreadystatechange = function() {
+                    if(!node.readyState || /loaded|complete/.test(node.readyState)) {
+                        node = node.onload = node.onreadystatechange = null;
+                        callback(200, "success");
+                    }
+                };
+
+                node.onerror = function(){
+                    $head[0].removeChild(node);
+                    node = node.onload = node.onreadystatechange = null;
+                    callback(404, "error");
+                };
+
+                $head[0].appendChild(node);
+                return true;
+            },
+
+            abort: function() {
+                node.onerror();
+            }
+        };
+    }
+    $.ajaxTransport('lazy_script', _lazyScriptTransport);
+
+    /**
+     * @param {Function|Array} stack
+     * @param {Array|Function} newCallback
+     * @returns {Array}
+     */
+    function _addCallback(stack, newCallback){
+
+        if(!stack) {
+            stack = [];
+        } else if(!Array.isArray(stack)) {
+            stack = [stack];
+        }
+
+        if(Array.isArray(newCallback)) {
+            stack = stack.concat(newCallback);
+        } else {
+            stack.push(newCallback);
+        }
+
+        return stack;
+    }
+
+    /**
+     * @param {Object} [options]
+     * @param {String} options.moduleDir
+     * @param {Boolean} options.addFromPage
+     *
+     * @returns {{load: load, pool: {}, loads: loads}}
+     */
+    var Loader = function(options) {
+        options = options || {};
+
+        const READY = 1;
+        const PENDING = 2;
+        const ERROR = 3;
+
+        options = $.extend({
+            moduleDir: '', // relpath
+            addFromPage: true
+        }, options);
+
+        var pool = {};
+
+        /**
+         * @param {String} name
+         * @returns {String}
+         */
+        function getUrl(name){
+            return (/\/\//.test(name)) ? name :  options.moduleDir + '/' + name; // [http(s):]//ya.ru/path.js
+        }
+
+        /**
+         * @param {String} name
+         * @param {Object} [params] - implement $.ajax(params) - http://stage.api.jquery.com/jQuery.ajax/
+         * @param {Function} [callback]
+         * @returns {Object} jqXHR
+         */
+        function load(name, params, callback) {
+            params = params || {};
+            if(typeof params === 'function') {
+                callback = params;
+                params = {};
+            }
+
+            var ext = _getExt(name);
+            var url = params.url || getUrl(name);
+
+            if(isLoad(name, url)) {
+                return false; // ready
+            }
+
+            if(getStatus(name) === PENDING){
+                return false; // loading
+            }
+
+            params.success = _addCallback(params.success, [
+                function(){
+                    pool[name] = {
+                        url: url,
+                        status: READY
+                    };
+                    console.log('load ' + name);
+                },
+                function(){
+                    $document.trigger('Loader.load', [name, ext, url]);
+                }
+            ]);
+
+            if(callback instanceof Function) {
+                params.success.unshift(callback);
+            }
+
+            params.error = _addCallback(params.error, function(){
+                console.log("Модуль " + name + " не был успешно загружен");
+                pool[name] = {
+                    status: ERROR
+                };
+            });
+
+            pool[name] = {
+                url: url,
+                status: PENDING
+            };
+
+            params = $.extend({
+                type: 'GET',
+                crossDomain: true,
+                cache: true,
+                url: url,
+                statusCode: {
+                    404: params.error
+                }
+            }, params);
+
+            params['dataType'] = 'lazy_script';
+
+            return $.ajax(params);
+        }
+
+        /**
+         * @param {String} name
+         * @param {String} url
+         * @returns {boolean}
+         */
+        function isLoad(name, url) {
+
+            if(pool[name] && pool[name]['status'] === READY) {
+                if(pool[name]['url'] && (pool[name]['url'] != url)) {
+                    console.log("Скрипт " + name + " был подгружен ранее, но с другого адреса " + pool[name]);
+                }
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * @param {String} name
+         * @returns {String|bool}
+         */
+        function getStatus(name){
+            return pool[name] ? pool[name]['status'] : false;
+        }
+
+        function addHavesModules(){
+            var css = $("link[href$='css']", $head);
+            var js = $("script[src$='js']", $head);
+            js.add(css).each(function() {
+                var url = $(this).attr('src') || $(this).attr('href');
+                pool[url] = {
+                    url: url,
+                    status: READY
+                };
+            });
+        }
+
+        /**
+         * @param  {Array} modules
+         * @returns {boolean}
+         */
+         function isLoads(modules) {
+            var i = modules.length;
+
+            while(i--) {
+                var module = modules[i];
+                var moduleName = module.name ||  module;
+                var moduleUrl = module.url || module;
+
+                if ( !isLoad(moduleName, moduleUrl) ) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /**
+         * @param {Array} modules
+         * @param {Function} callback
+         */
+        function loads(modules, callback){
+            if(isLoads(modules)) {
+                callback ? callback() : '';
+            } else {
+                for(var i = modules.length; i--;) {
+                    var module = modules[i];
+                    var name = module.name || module;
+                    var params = typeof module === 'string' ? {} : module;
+                    load(name, params);
+                }
+                var loadDepend = function(){
+                    if(isLoads(modules)) {
+                        $(document).off('Loader.load', loadDepend);
+                        callback();
+                    }
+                };
+
+                if(callback) {
+                    $(document).on('Loader.load', loadDepend);
+                }
+            }
+        }
+
+        if(options.addFromPage) {
+            addHavesModules();
+        }
+
+        return {
+            load: load,
+            pool: pool,
+            loads: loads
+        };
+    };
+
+    window.Loader = Loader;
+})(jQuery, window);
